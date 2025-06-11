f18620b1d4b06b3893c6103cb4c913ce6e9983a4
muzammil khan <muzammilnawaz2935337@gmail.com>
2025-06-11 09:59:48 +0500

    Update db.js

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ import { Pool } from "pg";
 import 'dotenv/config';
 
 const db = new Pool({
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    database:neondb,
+    user: neondb_owner,
+    password: npg_iHUT9egoD4yX,
+    host: ep-blue-frog-a86darz4-pooler.eastus2.azure.neon.tech,
+    port: 5432,
     ssl: {
         rejectUnauthorized: false, // Disable cert verification (fine for Neon/dev)
     },
